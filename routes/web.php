f36437552d79344aee7a5039cdf689a0cee527cf
gaskinMV <gaskinmark12@gmail.com>
2021-12-12 21:56:03 +0700

    v2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,41 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes([
+    'reset'=>false,
+    'confirm'=>false,
+    'verify'=>false,
+]);
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', 'App\Http\Controllers\MainController@index')->name('index');
+Route::get('/product/{productName}', 'App\Http\Controllers\MainController@product');
+Route::post('/basket/add/{id}','App\Http\Controllers\BasketController@basketAdd')->name('basket-add');
+Route::group([
+    'middleware' => 'basket_not_empty',
+    'prefix' => 'basket'
+],function()
+{
+    Route::get('/', 'App\Http\Controllers\BasketController@basket')->name('basket');
+    Route::get('/place', 'App\Http\Controllers\BasketController@basketPlace')->name('basket-place');
+    Route::post('/remove/{id}','App\Http\Controllers\BasketController@basketRemove')->name('basket-remove');
+    Route::post('/fullRemove/{id}','App\Http\Controllers\BasketController@basketFullRemove')->name('basket-fullRemove');
+    Route::post('/place','App\Http\Controllers\BasketController@basketConfirm')->name('basket-confirm');
 });
+
+
+Auth::routes();
+
+
+Route::group(['middleware' => 'auth'], function(){
+    Route::group(['middleware' => 'is_admin'], function (){
+        Route::get('/home/users', [App\Http\Controllers\HomeController::class, 'users'])->name('users');
+        Route::resource('users',\App\Http\Controllers\Auth\UserController::class );
+
+    });
+    Route::resource('orders',\App\Http\Controllers\Auth\OrderController::class );
+    Route::resource('devices',\App\Http\Controllers\Auth\DeviceController::class );
+    Route::get('/home/orders', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/home/products', [App\Http\Controllers\HomeController::class, 'products'])->name('products');
+});
+
